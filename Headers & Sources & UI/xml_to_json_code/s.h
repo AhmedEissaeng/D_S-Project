0d7aff77246a9753bb530d3ef42f3157addd4452
MohamedAlii59 <126531693+MohamedAlii59@users.noreply.github.com>
2024-01-07 21:47:51 +0200

    Add files via upload

---
+++ Headers & Sources & UI/xml_to_json_code/s.h
@@ -0,0 +1,20 @@
+#ifndef S_H
+#define S_H
+#include <QFileDialog>
+#include <QMessageBox>
+#include <QFile>
+#include <QTextStream>
+#include"QInputDialog"
+#include <QChar>
+#include "QtCore/qchar.h"
+#include <QString>
+
+using namespace std;
+
+
+#include "same_node.h"
+
+void s(QTextStream& , Node& , int , bool , bool );
+
+#endif // S_H
+
---
+++ Headers & Sources & UI/xml_to_json_code/same_node.h
@@ -0,0 +1,17 @@
+#ifndef SAME_NODE_H
+#define SAME_NODE_H
+
+#include <QFileDialog>
+#include <QMessageBox>
+#include <QFile>
+#include <QTextStream>
+#include"QInputDialog"
+#include <QChar>
+#include "QtCore/qchar.h"
+#include <QString>
+#include "Node.h"
+using namespace std;
+bool same_node(Node , Node );
+
+#endif // SAME_NODE_H
+
